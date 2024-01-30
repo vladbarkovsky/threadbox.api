ea4b5a36f31b2278646c23905da4c18fd4b8a00e
Vladislav Barkovsky <vladislav.barkovsky@bbit.tech>
2024-01-30 20:32:09 +0200

    (҂◡_◡)

--- ThreadboxApi/Web/WebConstants.cs
+++ ThreadboxApi/Web/WebConstants.cs
@@ -5,6 +5,6 @@
         /// <summary>
         /// HTTP request URL to get file by <see cref="ORM.Entities.FileInfo"/> <br/>
         /// </summary>
-        public const string FileUrl = "{0}/Files/GetFile?fileInfoId={1}";
+        public const string FileUrl = "{0}/api/Files/GetFile?FileInfoId={1}";
     }
 }
